a3cdc0bd3639accd027e1d43b6ea2da4eebd5318
Michael Volkmann <michael.volkmann2@vodafone.com>
2022-10-13 19:00:51 +0200

    Task 2

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,20 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build \
+    --build-arg HTTP_PROXY="http://192.168.56.1:3128" \
+    --build-arg HTTPS_PROXY="http://192.168.56.1:3128" \
+    -t udacity-mlm:latest \
+    .
 
 # Step 2: 
 # List docker images
+docker images --all
 
 # Step 3: 
 # Run flask app
+docker run \
+    -it \
+    --name udacity-machine-learning-microservice \
+    -p 8000:80 \
+    udacity-mlm:latest
