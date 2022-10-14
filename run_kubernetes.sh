b07bea16b636e3b6d98c8b44f299da39dcaea00f
Michael Volkmann <michael.volkmann2@vodafone.com>
2022-10-14 19:05:03 +0200

    Submission

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mivodu/udacity-mlm
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-mlm --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-mlm 8000:80
